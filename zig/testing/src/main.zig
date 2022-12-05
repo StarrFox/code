323a37a9828ae2fcb3e1e09e6f8ca9d1988b68ef
StarrFox <starrfox6312@gmail.com>
2022-12-04 22:44:34 -0600

    update

--- zig/testing/src/main.zig
+++ zig/testing/src/main.zig
@@ -1,9 +1,24 @@
 const std = @import("std");
 
+fn atest() u8 {
+    return 1;
+}
+
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    var value = atest();
+    std.log.info("value {d}", .{value});
 }
 
 test "basic test" {
     try std.testing.expectEqual(10, 3 + 7);
+}
+
+test "overflow" {
+    const value: u2 = 3;
+    try std.testing.expectEqual(0, value +% 1);
+}
+
+test "u3" {
+    const value: u3 = 7;
+    try std.testing.expectEqual(0, value +% 1);
 }
