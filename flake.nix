a7d5fe35718c5fa3cd4a6093310ee904cc378408
StarrFox <StarrFox6312@gmail.com>
2023-09-21 03:31:43 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "misc code";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nix-systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs @ {self, flake-parts, nix-systems, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      debug = true;
+      systems = import nix-systems;
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "code";
+          packages = with pkgs; [
+            python3
+            zig
+            cargo
+            gcc
+            nim
+            elixir
+          ];
+        };
+      };
+    };
+}
