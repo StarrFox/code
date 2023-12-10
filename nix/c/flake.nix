a37cae4a3a691eb05a65b4cedf4e90903e1a879a
StarrFox <StarrFox6312@gmail.com>
2023-12-10 06:37:16 +0000

    add basic c nix

---
+++ nix/c/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "a project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts/";
+    nix-systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs @ {flake-parts, nix-systems, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      debug = true;
+      systems = import nix-systems;
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "shell";
+          version = "0.1.0";
+          src = ./.;
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "shell";
+          packages = with pkgs; [
+            gnumake
+            autoconf
+            automake
+          ];
+        };
+      };
+    };
+}
