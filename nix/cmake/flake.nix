581f37d7a4ce4fac5013ebcfde147e062e25c7d1
StarrFox <StarrFox6312@gmail.com>
2023-12-10 08:24:43 +0000

    add basic cmake build

---
+++ nix/cmake/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "a project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts/";
+    nix-systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs @ {flake-parts, nix-systems, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      debug = true;
+      systems = import nix-systems;
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "test";
+          version = "0.1.0";
+          src = ./.;
+          nativeBuildInputs = [pkgs.cmake];
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "cmake";
+          packages = with pkgs; [
+            cmake
+          ];
+        };
+      };
+    };
+}
