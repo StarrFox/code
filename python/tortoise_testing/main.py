9317939f69c52b347f78c05902806a279b8ad349
StarrFox <StarrFox6312@gmail.com>
2025-08-11 19:09:27 -0500

    update

---
+++ python/tortoise_testing/main.py
@@ -0,0 +1,22 @@
+from tortoise.models import Model
+from tortoise import fields
+
+class Tournament(Model):
+    # Defining `id` field is optional, it will be defined automatically
+    # if you haven't done it yourself
+    id = fields.IntField(primary_key=True)
+    name = fields.CharField(max_length=255)
+
+
+class Event(Model):
+    id = fields.IntField(primary_key=True)
+    name = fields.CharField(max_length=255)
+    # References to other models are defined in format
+    # "{app_name}.{model_name}" - where {app_name} is defined in the tortoise config
+    tournament = fields.ForeignKeyField('models.Tournament', related_name='events')
+    participants = fields.ManyToManyField('models.Team', related_name='events', through='event_team')
+
+
+class Team(Model):
+    id = fields.IntField(primary_key=True)
+    name = fields.CharField(max_length=255)
